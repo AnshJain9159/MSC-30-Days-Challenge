b82c4079ac55a1e820fde003e61305443150a2e8
Ansh Jain <114240845+AnshJain9159@users.noreply.github.com>
2023-09-28 12:49:07 +0530

    Create Leetcode_Daily_28Sept.cpp

---
+++ Leetcode_Daily_28Sept.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = n-1;
+        if(n==1) return nums;
+        while(i<=j){
+            if((nums[i]%2!=0) && (nums[j]%2==0)){
+                swap(nums[i],nums[j]); //swapping if nums[i] is even and nums[j] is odd
+                i++;
+                j--;
+            }
+           else if(nums[i]%2==0) i++; //only moving forward if nums[j] was not odd
+            else if(nums[j]%2!=0) j--; //only moving backward if nums[i] was not even
+        }
+            return nums;
+    }
+};
