db55af531d3ea18b04a5ee7901c618983fdf549e
Ansh Jain <114240845+AnshJain9159@users.noreply.github.com>
2023-09-29 11:29:48 +0530

    Create Leetcode_Daily_29_Sept.cpp

---
+++ Leetcode_Daily_29_Sept.cpp
@@ -0,0 +1,15 @@
+/* https://leetcode.com/problems/monotonic-array/ */ 
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        vector<int>inc = nums;
+        vector<int>dec = nums;
+      //sorting both the vectors and reversing the decreasing order one.
+        sort(inc.begin(),inc.end()); 
+        sort(dec.begin(),dec.end());
+        reverse(dec.begin(),dec.end());
+      //checking if there is no peak element in between the vector,than we should return true else false.
+        if((nums == inc) || (nums==dec)) return true;
+        return false;
+    }
+};
