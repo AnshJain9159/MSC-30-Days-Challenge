1b6f6931959d74e19234cde3e4a03682e97df646
Ansh Jain <anshjain9159@gmail.com>
2023-10-02 11:35:28 +0530

    Leetcode_Daily_2_Oct.cpp
    Problem Link:
    https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/

---
+++ Leetcode_Daily/Leetcode_Daily_2_Oct.cpp
@@ -0,0 +1,28 @@
+/* Proble Link */
+/* https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/ */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        //initializing the counter variable
+        int countA=0;
+        int countB=0;
+        for(int i=1;i<colors.size();i++){
+            //matching condition for A and B
+            if(colors[i-1] == colors[i] && colors[i+1] == colors[i]){
+                if(colors[i] =='A'){
+                    countA++; // alice turns
+                }
+                //for B
+                else{
+                    countB++; //bob turns
+                }
+            }
+        }
+        // will return true if (countA>countB) else false
+        return countA>countB;
+    }
+};
